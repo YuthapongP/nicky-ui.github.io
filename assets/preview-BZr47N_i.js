5fad88ce2a9b0634fc25c28ccfdb2b4ae8d47900
yuthapongp <yuthapong@magicboxsolution.com>
2024-08-19 17:35:22 +0700

    first commit

---
+++ assets/preview-BZr47N_i.js
@@ -0,0 +1,1 @@
+const a={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},backgrounds:{default:"dark",values:[{name:"light",value:"#ffffff"},{name:"dark",value:"#333333"},{name:"blue",value:"#00f"}]}}};export{a as default};
